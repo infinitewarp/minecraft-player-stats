1301e260b338eb28441581fba1e9dd3699961871
Brad Smith <bradster@infinitewarp.com>
2014-03-26 20:08:04 -0400

    add comment about virtualenv to deal with some distros having multiple python installations

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+# If you're on a system with multiple Python installations, you may
+# need to change "virtualenv" to something like "virtualenv-2.6".
 virtualenv flaskenv
 PIP=flaskenv/bin/pip
 $PIP install flask
